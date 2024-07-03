17136191fc49bc2f0edff6d51ff00d4e610c685e
gerardao7 <gerardao7@gmail.com>
2024-07-03 23:05:14 +0200

    Cambios

---
+++ variables.js
@@ -0,0 +1,21 @@
+// usernamen es una variable que tiene dentro una string
+username = "Gerardo";
+userSurname = "Alvarez";
+
+
+userTitle = "Diseñador";
+
+console.log("userName");
+console.log("userSurname");
+console.log("userTitle");
+
+console.log(userName + userTitle + userSurname)
+console.log(userName + " " + userTitle + " " + userSurname)
+console.log(userName + espacio + userSurname + espacio + userSurname)
+
+console.log("¿Que tal estas, $(username)?");
+
+userAge = 33; //Number
+userHasHair = false; //Boolean
+
+//TODO: Objects
